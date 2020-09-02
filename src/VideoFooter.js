ea242fcf201b5e209b40352dee3f447616332889
MCK <kckmehmet@icloud.com>
2020-09-03 00:38:17 +0300

    update video footer

--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './VideoFooter.css';
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import Ticker from 'react-ticker';
 
 function VideoFooter() {
   return (
@@ -7,7 +9,21 @@ function VideoFooter() {
       <div className="videoFooter__text">
         <h3>@ssssanhga</h3>
         <p>This is some description</p>
+        <div className="videoFooter__ticker">
+          <MusicNoteIcon className="videoFooter__icon" />
+          <Ticker mode="smooth">
+            {({ index }) => (
+              <>
+                <p>I am a song</p>
+              </>
+            )}
+          </Ticker>
+        </div>
       </div>
+      <img
+        className="videoFooter__record"
+        src="https://static.thenounproject.com/png/934821-200.png"
+      />
     </div>
   );
 }
