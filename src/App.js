e52c49ae667064ef242d94799a8f02bbd3ae8f03
MCK <kckmehmet@icloud.com>
2020-09-06 19:56:54 +0300

    update props

--- src/App.js
+++ src/App.js
@@ -7,8 +7,24 @@ function App() {
     //BEM naming convantion
     <div className="app">
       <div className="app__videos">
-        <Video />
-        <Video />
+        <Video
+          url="https://v16m.tiktokcdn.com/c80818f600134e30df549f3bcfcb60da/5f554bf4/video/tos/useast2a/tos-useast2a-pve-0068/9c48b59a2d894391927eb038f05a824f/?a=1233&br=3368&bt=1684&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20200906145155010190208043070724A2&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=Mzgzd29weHVvdjMzZTczM0ApN2hkaTdpaWU7NzloOWk5M2dscWRpXl5xZm5fLS1fMTZzczQxLWM2NDExMzVhMTQtNmM6Yw%3D%3D&vl=&vr="
+          channel="mehmetcan"
+          description="MERN STACK Developer"
+          song="Sen affetsen ben affetmem"
+          likes={123}
+          messages={11}
+          shares={23}
+        />
+        <Video
+          url="https://v16m.tiktokcdn.com/c80818f600134e30df549f3bcfcb60da/5f554bf4/video/tos/useast2a/tos-useast2a-pve-0068/9c48b59a2d894391927eb038f05a824f/?a=1233&br=3368&bt=1684&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20200906145155010190208043070724A2&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=Mzgzd29weHVvdjMzZTczM0ApN2hkaTdpaWU7NzloOWk5M2dscWRpXl5xZm5fLS1fMTZzczQxLWM2NDExMzVhMTQtNmM6Yw%3D%3D&vl=&vr="
+          channel="mehmetcan"
+          description="MERN STACK Developer"
+          song="Sen affetsen ben affetmem"
+          likes={123}
+          messages={11}
+          shares={23}
+        />
       </div>
 
       {/* app container */}
--- src/Video.js
+++ src/Video.js
@@ -1,8 +1,9 @@
 import React, { useRef, useState } from 'react';
 import './Video.css';
 import VideoFooter from './VideoFooter';
+import VideoSideBar from './VideoSidebar';
 
-function Video() {
+function Video({ url, channel, description, song, likes, messages, shares }) {
   const [playing, setPlaying] = useState(false);
   const videoRef = useRef(null);
 
@@ -28,10 +29,11 @@ function Video() {
         className="video__player"
         loop
         ref={videoRef}
-        src="https://v77.tiktokcdn.com/c184e12032244c18186dd5985ea14a12/5f503e18/video/tos/alisg/tos-alisg-pve-0037/4ca742d43bd942e8b3c7885b9f5155d6/?a=1233&br=3310&bt=1655&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20200902185058010189065105341F3237&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M2tlOXBueGpxdzMzZDczM0ApZ2RnM2QzZ2RoNzQ3aTloOGdibGhwYWc2LjNfLS00MTRzc2FfM2IyXl5hMi82NmAtMC06Yw%3D%3D&vl=&vr="
+        src={url}
       ></video>
 
-      <VideoFooter />
+      <VideoFooter channel={channel} description={description} song={song} />
+      <VideoSideBar likes={likes} shares={shares} messages={messages} />
       {/* Video SideBar */}
     </div>
   );
