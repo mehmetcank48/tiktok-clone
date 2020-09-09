cb25ef2eca3a7843a9a5d3132488c5fcd450af49
MCK <kckmehmet@icloud.com>
2020-09-10 01:08:06 +0300

    update tiktok clone

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Video from './Video';
+import axios from './axios';
 
 function App() {
+
+  const [videos, setVideos] = useState([])
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await axios.get('/v2/posts');
+      setVideos(response.data);
+
+      return response;
+    }
+
+    fetchPosts();
+  }, []);
+
+  console.log(videos);
+
   return (
     //BEM naming convantion
     <div className="app">
       <div className="app__videos">
-        <Video
-          url="https://v16m.tiktokcdn.com/c80818f600134e30df549f3bcfcb60da/5f554bf4/video/tos/useast2a/tos-useast2a-pve-0068/9c48b59a2d894391927eb038f05a824f/?a=1233&br=3368&bt=1684&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20200906145155010190208043070724A2&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=Mzgzd29weHVvdjMzZTczM0ApN2hkaTdpaWU7NzloOWk5M2dscWRpXl5xZm5fLS1fMTZzczQxLWM2NDExMzVhMTQtNmM6Yw%3D%3D&vl=&vr="
-          channel="mehmetcan"
-          description="MERN STACK Developer"
-          song="Sen affetsen ben affetmem"
-          likes={123}
-          messages={11}
-          shares={23}
-        />
-        <Video
-          url="https://v16m.tiktokcdn.com/c80818f600134e30df549f3bcfcb60da/5f554bf4/video/tos/useast2a/tos-useast2a-pve-0068/9c48b59a2d894391927eb038f05a824f/?a=1233&br=3368&bt=1684&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20200906145155010190208043070724A2&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=Mzgzd29weHVvdjMzZTczM0ApN2hkaTdpaWU7NzloOWk5M2dscWRpXl5xZm5fLS1fMTZzczQxLWM2NDExMzVhMTQtNmM6Yw%3D%3D&vl=&vr="
-          channel="mehmetcan"
-          description="MERN STACK Developer"
-          song="Sen affetsen ben affetmem"
-          likes={123}
-          messages={11}
-          shares={23}
-        />
+        {videos.map(({ url, channel, description, song, likes, messages, shares }) => {
+          return <Video
+            url={url}
+            channel={channel}
+            description={description}
+            song={song}
+            likes={likes}
+            messages={messages}
+            shares={shares}
+          />
+        })
+        }
       </div>
-
-      {/* app container */}
-      {/* videos */}
-      {/* <Video /> */}
-      {/* <Video /> */}
-      {/* <Video /> */}
-      {/* <Video /> */}
-      {/* <Video /> */}
     </div>
   );
 }
